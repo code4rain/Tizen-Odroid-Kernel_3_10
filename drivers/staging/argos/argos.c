013542c6ced303229f8e7fb5bdd3681881a2f629
Alex Kyoungdon Jang <alex.jang@samsung.com>
2015-06-03 17:12:10 +0900

    argos: implement parse_dt
    Signed-off-by: Alex Kyoungdon Jang <alex.jang@samsung.com>

--- drivers/staging/argos/argos.c
+++ drivers/staging/argos/argos.c
@@ -21,16 +21,86 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/of.h>
+#include <linux/pm_qos.h>
 
 #define ARGOS_NAME "argos"
 
-struct argos_platform_data {
+struct boost_entry {
+	unsigned int throughput;
+	unsigned int cpu_freq;
+	unsigned int bus_freq;
 };
+
+struct argos_pm_qos {
+	struct pm_qos_request cpu_qos_req;
+	struct pm_qos_request bus_qos_req;
+};
+
+struct argos_block {
+	const char *name;
+	struct platform_device *pdev;
+	struct boost_entry *table;
+	int num_entry;
+	int prev_level;
+	struct argos_pm_qos *qos;
+};
+
+struct argos_platform_data {
+	struct argos_block *blocks;
+	int nblocks;
+};
+
 static struct argos_platform_data *argos_pdata;
 
 #ifdef CONFIG_OF
 static int argos_parse_dt(struct device *dev)
 {
+	struct argos_platform_data *pdata = dev->platform_data;
+	struct argos_block *block;
+	struct device_node *np, *cnp;
+	int blk_count = 0;
+
+	np = dev->of_node;
+	pdata->nblock = of_get_child_count(np);
+	if (!pdata->nblock)
+		return -ENODEV;
+
+	pdata->blocks = devm_kzalloc(dev, sizeof(struct argos_block) * pdata->nblock,
+			GFP_KERNEL);
+	if (!pdata->blocks)
+		return -ENOMEM;
+
+	for_each_child_of_node(np, cnp) {
+		int num_entry;
+		int i;
+		block = &pdata->blocks[blk_count];
+		block->name = of_get_property(cnp, "net_boost,label", NULL);
+		if (of_get_property_read_u32(cnp, "net_boost,table_size", &num_entry))
+			return -EINVAL;
+		block->num_entry = num_entry;
+
+		block->table = devm_kzalloc(dev, sizeof(struct boost_entry) * num_entry,
+				GFP_KERNEL);
+		if (!block->table)
+			return -ENOMEM;
+		for (i = 0; i < num_entry; i++) {
+			if (of_property_read_u32_index(cnp, "net_boost,table", i * 3,
+						&block->table[i].throughput))
+				return -EINVAL;
+			if (of_property_read_u32_index(cnp, "net_boost,table", i * 3 + 1,
+						&block->table[i].cpu_freq))
+				return -EINVAL;
+			if (of_property_read_u32_index(cnp, "net_boost,table", i * 3 + 2,
+						&block->table[i].bus_freq))
+				return -EINVAL;
+		}
+		block->prev_level = -1;
+		block->qos = devm_kzalloc(dev, sizeof(struct argos_pm_qos), GFP_KERNEL);
+		if (!block->qos)
+			return -ENOMEM;
+		blk_count++;
+	};
+
 	return 0;
 }
 #endif
